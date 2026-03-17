65e3a3b8d7089b522b91603102ea71449c4bc70e
rh64s <cchelovekov64@gmail.com>
2026-03-17 21:13:06 +0700

    upd: comments

--- core/Src/Route.php
+++ core/Src/Route.php
@@ -24,7 +24,8 @@ class Route
     public function start(): void
     {
         $path = explode('?', $_SERVER['REQUEST_URI'])[0];
-        $path = substr($path, 1, strlen(self::$prefix)-1);
+        $path = substr($path, 1, strlen(self::$prefix)-1); // было $path = substr($path, strlen(self::$prefix) + 1);
+        // отсюда требовалось убрать '/' в пути, однако вероятно substr с новой версией php изменился, и в итоге получали пустую строку.
         if (!array_key_exists($path, self::$routes)) {
             throw new Error('This path does not exist');
         }
