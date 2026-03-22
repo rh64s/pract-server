a9d795478e6d92b1b3fe8bd579d9f45b68954a07
rh64s <cchelovekov64@gmail.com>
2026-03-22 23:23:29 +0700

    refactor: create action

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -2,6 +2,7 @@
 
 namespace Src;
 
+use App\Actions\CreateUserIfDoenstExists;
 use Error;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
@@ -52,17 +53,7 @@ class Application
         $this->dbManager->setEventDispatcher(new Dispatcher(new Container));
         $this->dbManager->setAsGlobal();
         $this->dbManager->bootEloquent();
-        if (User::all()->count() === 0) {
-            User::create([
-                'name' => 'Админ',
-                'surname' => 'Админович',
-                'login' => 'admin',
-                'password' => 'admin',
-                'role_id' => 1,
-                'email' => 'admin@example.com',
-                'phone' => '88005553535',
-            ]);
-        }
+        CreateUserIfDoenstExists::execute();
     }
 
     public function run(): void
